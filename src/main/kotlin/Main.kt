96beee6a624914841d7daf82309f81e05c99d569
Swapnil <swapnil,\.bhojwani789@gmail.com>
2022-11-03 23:18:57 +0530

    Now getting the thumbnail url from linkedin post

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,17 @@
 import org.jsoup.Jsoup
 
 fun main() {
-    val doc =
-        Jsoup.connect("https://www.linkedin.com/posts/saheli-chatterjee-7b3023183_sales-tools-ugcPost-6987375370069032960-IxAG?utm_source=share&utm_medium=member_android")
-    val mainContent = doc.get().getElementById("main-content")
-    val videoSrc = mainContent
-        ?.getElementsByClass("core-rail mx-auto papabear:w-core-rail-width mamabear:max-w-[790px] babybear:max-w-[790px]")!![0].allElements[1].getElementsByClass(
-        "share-native-video"
-    )?.first()
-        ?.getElementsByClass("share-native-video__node video-js")?.attr("data-sources")
-    println(videoSrc?.split(",")?.first()?.removeRange(0, 8)?.removeSurrounding("\""))
-    println(getLinkFromLinkedUrlOptimized("https://www.linkedin.com/posts/saheli-chatterjee-7b3023183_sales-tools-ugcPost-6987375370069032960-IxAG?utm_source=share&utm_medium=member_android"))
+    /*    val doc =
+            Jsoup.connect("https://www.linkedin.com/posts/saheli-chatterjee-7b3023183_sales-tools-ugcPost-6987375370069032960-IxAG?utm_source=share&utm_medium=member_android")
+        val mainContent = doc.get().getElementById("main-content")
+        val videoSrc = mainContent
+            ?.getElementsByClass("core-rail mx-auto papabear:w-core-rail-width mamabear:max-w-[790px] babybear:max-w-[790px]")!![0].allElements[1].getElementsByClass(
+            "share-native-video"
+        )?.first()
+            ?.getElementsByClass("share-native-video__node video-js")?.attr("data-sources")
+        println(videoSrc?.split(",")?.first()?.removeRange(0, 8)?.removeSurrounding("\""))*/
+    println(getLinkFromLinkedUrlOptimized("https://www.linkedin.com/posts/connectwell-industries-pvt-ltd_wherever-you-need-electrical-connections-activity-6971128396147798017-sjSf?utm_source=share&utm_medium=member_desktop"))
+    println(getThumbnailOfLinkedInVideo("https://www.linkedin.com/posts/connectwell-industries-pvt-ltd_wherever-you-need-electrical-connections-activity-6971128396147798017-sjSf?utm_source=share&utm_medium=member_desktop"))
 }
 
 fun getLinkFromLinkedUrlOptimized(linkedInUrl: String): String? {
@@ -19,4 +20,13 @@ fun getLinkFromLinkedUrlOptimized(linkedInUrl: String): String? {
     val mainContent = doc.get().getElementById("main-content")
     val videoSrc = mainContent?.firstElementChild()?.firstElementChild()?.select("video")?.attr("data-sources")
     return videoSrc?.split(",")?.first()?.removeRange(0, 8)?.removeSurrounding("\"")
+}
+
+fun getThumbnailOfLinkedInVideo(linkedInUrl: String): String? {
+    val doc =
+        Jsoup.connect(linkedInUrl)
+    val mainContent = doc.get().getElementById("main-content")
+    val thumbUrl =
+        mainContent?.firstElementChild()?.firstElementChild()?.select("video")?.first()?.attr("data-poster-url")
+    return thumbUrl
 }
